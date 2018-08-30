57a0ed7de73f80835f680f58d662a724ec2a4a5e
ag83 <andrey.grisch@gmail.com>
2018-08-30 17:53:55 +0300

    fix https marvel api

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,6 @@
 const constants = {
   marvelApiKey: 'f62d8a81b3d0002951a5315e21028eb1',
-  marvelApi: 'http://gateway.marvel.com/v1/public/'
+  marvelApi: 'https://gateway.marvel.com/v1/public/'
 };
 
 export default constants;
